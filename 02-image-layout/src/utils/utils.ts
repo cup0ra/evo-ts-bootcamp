e0cfce6ebdab3c99f157dfd21764b4d0f087643e
cup0ra <cup0ra@users.noreply.github.com>
2021-04-30 23:01:06 +0300

    fix: api key

--- 02-image-layout/src/utils/utils.ts
+++ 02-image-layout/src/utils/utils.ts
@@ -2,7 +2,7 @@ import { IImage } from '../models/model';
 
 export const getImages = async (search: string): Promise<IImage[]> => {
   const response = await fetch(
-    `https://api.unsplash.com/search/photos?per_page=30&client_id=n0rS0YBba_6PCwRYTtsw9bNQuOPFBcefP2kG8i-5NKw&query=${search}`,
+    `https://api.unsplash.com/search/photos?per_page=30&client_id=${process.env.REACT_APP_API_KEY}&query=${search}`,
   );
   const arrayPhoto = await response.json();
   return arrayPhoto.results.map((image: any) => {
